2902755722ae44d1bc69ac784189da754f9a4c32
Roelas <72160242+Roelas@users.noreply.github.com>
2020-10-06 20:31:37 +0200

    Update CrearArchivo.java

--- CrearArchivo.java
+++ CrearArchivo.java
@@ -1,23 +1,39 @@
-import java.io.BufferedWriter;
 import java.io.File;
+import java.util.Scanner;
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class CrearArchivo {
 
-	 public static void main(String[] args) throws IOException {
-	        String ruta = "/home/mario/archivo.txt";
-	        File archivo = new File(ruta);
-	        BufferedWriter bw;
-	        if(archivo.exists()) {
-	            bw = new BufferedWriter(new FileWriter(archivo));
-	            bw.write("El fichero de texto ya estaba creado.");
-	        } else {
-	            bw = new BufferedWriter(new FileWriter(archivo));
-	            bw.write("Acabo de crear el fichero de texto.");
-	        }
-	        bw.close();
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void Crear() throws IOException {
+		
+		try {
+			
+			Scanner teclado = new Scanner(System.in);
+			System.out.println("Escribe la ruta absoluta incluyendo el nombre del archivo que quieres crear.");
+			System.out.println("EJEMPLO: 'C:/../../../archivo.txt");
+			String ruta = teclado.nextLine();
+			File archivo = new File(ruta);
+			BufferedWriter bw;
+			    
+			if(archivo.exists()) {
+				bw = new BufferedWriter(new FileWriter(archivo));
+			    System.out.println("Error, el archivo ya existe");
+			} else {
+			    bw = new BufferedWriter(new FileWriter(archivo));
+			    System.out.println("El archivo ha sido creado correctamente.");
+			}
+			
+			bw.close();
+			
+		} catch(Exception e) {
+			System.out.println(e);
 	    }
-	    
 	}
 
+}
