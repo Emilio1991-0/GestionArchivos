165c39de5c59d515b19ff5bb977dd572da799f12
Roelas <72160242+Roelas@users.noreply.github.com>
2020-10-06 20:31:58 +0200

    Update EliminarArchivo.java

--- EliminarArchivo.java
+++ EliminarArchivo.java
@@ -1,31 +1,35 @@
 import java.io.File;
+import java.util.Scanner;
 
 public class EliminarArchivo {
 
-    public static void Eliminar(String args[]){
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void Eliminar() {
+		
+		try {
 
-        try{
-
-            File archivo = new File("C:\\practicas\\Ejercicio1.txt");
+			Scanner teclado = new Scanner(System.in);
+			System.out.println("Escribe la ruta absoluta incluyendo el nombre del archivo que quieres eliminar.");
+			System.out.println("EJEMPLO: 'C:/../../../archivo.txt");
+			String ruta = teclado.nextLine();
+			File archivo = new File(ruta);
 
             boolean estatus = archivo.delete();
 
             if (!estatus) {
+                System.out.println("Error, no se ha podido eliminar el archivo");
+                
+            } else {
+                System.out.println("Se ha eliminado el archivo correctamente");
+            }
 
-                System.out.println("Error no se ha podido eliminar el  archivo");
-
-           }else{
-
-                System.out.println("Se ha eliminado el archivo exitosamente");
-
-           }
-
-        }catch(Exception e){
-
-           System.out.println(e);
-
+        } catch(Exception e) {
+        	System.out.println(e);
         }
-
-    }
-
+		
+	}
+	
 }
