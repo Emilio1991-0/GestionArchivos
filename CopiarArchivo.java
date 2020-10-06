6c1d7b7ad7418f788f021e024b113f757252b214
Roelas <72160242+Roelas@users.noreply.github.com>
2020-10-06 21:04:18 +0200

    Update CopiarArchivo.java

--- CopiarArchivo.java
+++ CopiarArchivo.java
@@ -1,4 +1,3 @@
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -8,25 +7,29 @@ import java.io.OutputStream;
 import java.util.Scanner;
 
 public class CopiarArchivo {
-
+	
 	static Scanner teclado = new Scanner(System.in);
 	private static File origen;
 	private static File destino;
 	private static InputStream in;
 	private static OutputStream out;
 	
-	public boolean copiarFichero() {
-		System.out.println("Introduce el path absoluto del archivo a copiar:");
+	public static boolean Copiar() {
+		
+		System.out.println("Escribe la ruta absoluta incluyendo el nombre del archivo que quieres copiar.");
+		System.out.println("EJEMPLO: 'C:/../../../archivo.txt");
 		String pathOrigen = teclado.nextLine();
-		System.out.println("Introduce el path absoluto donde quieres copiar:");
+		System.out.println("Escribe la ruta absoluta incluyendo el nombre donde quieres copiar el archivo.");
+		System.out.println("EJEMPLO: 'C:/../../../archivo.txt");
 		String pathDestino = teclado.nextLine();
+		
 		origen = new File(pathOrigen);
         destino = new File(pathDestino);
+        
         if (origen.exists()) {
             try {
                 InputStream in = new FileInputStream(origen);
                 OutputStream out = new FileOutputStream(destino);
-                // Usamos un buffer para la copia.
                 byte[] buf = new byte[1024];
                 int len;
                 while ((len = in.read(buf)) > 0) {
